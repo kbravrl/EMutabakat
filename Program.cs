761902b1737729414e1dbf449e0bf6affa36fd7a
kbravrl <kubravarol499@gmail.com>
2026-03-23 09:50:10 +0300

    Apply ef core migrations on startup for docker postgres

--- Program.cs
+++ Program.cs
@@ -1,31 +1,53 @@
 using EMutabakat.Components;
 using EMutabakat.Data;
+using EMutabakat.Services;
+using EMutabakat.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddRadzenComponents();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IFirmaService, FirmaService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Try a few times until PostgreSQL in Docker is ready.
+for (int i = 0; i < 10; i++)
+{
+    try
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        dbContext.Database.Migrate();
+        break;
+    }
+    catch
+    {
+        if (i == 9) throw;
+        Thread.Sleep(3000);
+    }
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
